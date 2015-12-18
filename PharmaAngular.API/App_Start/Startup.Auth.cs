9390dc651ee978708e15bc4ff981579ba54284b0
chnous <julo-88@hotmail.fr>
2015-12-18 09:17:07 +0100

    change taken livetime to 24 hours

--- PharmaAngular.API/App_Start/Startup.Auth.cs
+++ PharmaAngular.API/App_Start/Startup.Auth.cs
@@ -36,7 +36,7 @@ namespace PharmaAngular.API
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 //AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(24),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
