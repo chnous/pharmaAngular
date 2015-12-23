6181984b125f027c24398ab12b66a8df43fb610e
chnous <julo-88@hotmail.fr>
2015-12-23 16:51:18 +0100

    original bootstrap

--- PharmaAngular.API/Controllers/ProductController.cs
+++ PharmaAngular.API/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 
 namespace PharmaAngular.API.Controllers
@@ -34,6 +35,11 @@ namespace PharmaAngular.API.Controllers
                 new Product {ProductId = 0,  Name = "toto" },
                 new Product { ProductId = 1, Name = "titi" }
             };
+            for (int i = 2; i < 500; i++)
+            {
+                productList.Add(new Product { ProductId = i, Name = "test" + i });
+                Thread.Sleep(10);
+            }
 
             return productList;
 
